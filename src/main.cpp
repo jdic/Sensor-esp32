bad9e68396b954b669d523e0dd4c9cdd2f3450ec
jdi <jmotadv@gmail.com>
2024-05-27 20:23:07 -0700

    [FIXES] FAST FIXES

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include <ESPAsyncWebServer.h>
+#include <DallasTemperature.h>
+#include <OneWire.h>
 #include <Arduino.h>
 #include <WiFi.h>
 
-#define ENG 16
+#define ONE_WIRE_BUS 2
+#define ENA 15
+#define IN1 16
+#define IN2 17
 
 AsyncWebServer server(80);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 
-const char* ssid = "ESP32";
-const char* password = "unodostres";
+const char* ssid = "HUAWEI-0617AJ";
+const char* password = "rgdrcVwrBcLf9tuMpF";
 
-bool isActiveEngine = false;
 bool isConnected = false;
+int mappedSpeed = 0;
 
 void blink(int speed)
 {
@@ -22,25 +29,64 @@ void blink(int speed)
 
 float handleSensor(int PIN)
 {
-  float value = analogRead(PIN);
-
-  return value;
+  sensors.requestTemperatures();
+  return sensors.getTempFByIndex(0);
 }
 
-void handleEngineHTTP()
+void handleEngine(bool state, int speed)
 {
+  if (state)
+  {
+    digitalWrite(ENA, HIGH);
+    analogWrite(ENA, speed);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  }
+
+  else
+  {
+    digitalWrite(ENA, LOW);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+  }
+}
+
+void handleEngineHTTP() {
   server.on("/engine/on", HTTP_POST, [](AsyncWebServerRequest *request)
   {
     Serial.println(F("[ENGINE] [ON]"));
-    digitalWrite(ENG, LOW);
-    request->send(200);
+    handleEngine(true, mappedSpeed);
+    AsyncWebServerResponse *response = request->beginResponse(200);
+    response->addHeader("Access-Control-Allow-Origin", "*");
+    request->send(response);
   });
 
   server.on("/engine/off", HTTP_POST, [](AsyncWebServerRequest *request)
   {
     Serial.println(F("[ENGINE] [OFF]"));
-    digitalWrite(ENG, LOW);
-    request->send(200);
+    handleEngine(false, 0);
+    AsyncWebServerResponse *response = request->beginResponse(200);
+    response->addHeader("Access-Control-Allow-Origin", "*");
+    request->send(response);
+  });
+
+  server.on("/engine/speed", HTTP_POST, [](AsyncWebServerRequest *request)
+  {
+    if (request->hasParam("speed", true))
+    {
+      int speed = request->getParam("speed", true)->value().toInt();
+      Serial.print(F("[ENGINE] [SPEED] "));
+      Serial.println(speed);
+      mappedSpeed = map(speed, 0, 100, 0, 255);
+      AsyncWebServerResponse *response = request->beginResponse(200);
+      response->addHeader("Access-Control-Allow-Origin", "*");
+      request->send(response);
+    }
+
+    else
+    {
+      request->send(400, "text/plain", "Missing speed parameter");
+    }
   });
 }
 
@@ -48,12 +94,33 @@ void handleSensorHTTP()
 {
   server.on("/sensor", HTTP_GET, [](AsyncWebServerRequest *request)
   {
-    int PIN = request->getParam("pin")->value().toInt();
-    Serial.print(F("[SENSOR] [READ] "));
-    Serial.println(PIN);
+    if (request->hasParam("pin")) {
+      int PIN = request->getParam("pin")->value().toInt();
+      Serial.print(F("[SENSOR] [READ] "));
+      Serial.println(PIN);
+      String sensorValue = String(handleSensor(PIN));
+      AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", sensorValue);
+      response->addHeader("Access-Control-Allow-Origin", "*");
+      request->send(response);
+    }
 
-    request->send(200, "text/plain", String(handleSensor(PIN)));
+    else
+    {
+      request->send(400, "text/plain", "Missing pin parameter");
+    }
   });
+}
+
+void addCORSHeaders(AsyncWebServerRequest *request)
+{
+  if (request->method() == HTTP_OPTIONS)
+  {
+    AsyncWebServerResponse *response = request->beginResponse(204);
+    response->addHeader("Access-Control-Allow-Origin", "*");
+    response->addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    response->addHeader("Access-Control-Allow-Headers", "Content-Type");
+    request->send(response);
+  }
 }
 
 void initWiFi()
@@ -65,14 +132,19 @@ void initWiFi()
 
 void WiFiEventHandler(WiFiEvent_t event)
 {
-  switch (event) 
-  {
+  switch (event) {
     case SYSTEM_EVENT_STA_GOT_IP:
       isConnected = true;
       Serial.print(F("[INFO] [ADDRESS] "));
       Serial.println(WiFi.localIP());
       handleEngineHTTP();
       handleSensorHTTP();
+      server.onNotFound([](AsyncWebServerRequest *request)
+      {
+        addCORSHeaders(request);
+        request->send(404, "text/plain", "Not found");
+      });
+      server.on("/", HTTP_OPTIONS, addCORSHeaders);
       server.begin();
       break;
 
@@ -94,8 +166,13 @@ void setup()
   Serial.println(F("Sensor Reader"));
   Serial.println();
 
-  pinMode(ENG, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
 
   initWiFi();
 }
@@ -106,7 +183,7 @@ void loop()
   {
     digitalWrite(LED_BUILTIN, HIGH);
   }
-
+  
   else
   {
     blink(800);
